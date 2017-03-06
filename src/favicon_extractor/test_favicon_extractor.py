ffe34acfe27fe0ea8db4fb383c57f482dbcf6c1d
Mark Anderson <mark@m3b.net>
2017-03-06 08:30:39 +0100

    commenting out some stuff for now

--- src/favicon_extractor/test_favicon_extractor.py
+++ src/favicon_extractor/test_favicon_extractor.py
@@ -3,11 +3,13 @@ import unittest
 from fixtures import *
 from favicon_extractor import get_favicon
 
+
 class TestFaviconMethods(unittest.TestCase):
 
+    """
     def test_has_no_favicon_defined_and_no_favicon_on_server(self):
         result = get_favicon('amazon.com')
-
+    """
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
