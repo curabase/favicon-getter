4a9454973eb7b7bd1cc3a9a72fd21d55e217413f
Mark Anderson <mark.anderson@quinyx.com>
2017-03-06 16:58:38 +0100

    added stubs for tests that we need to write

--- src/favicon_extractor/test_favicon_extractor.py
+++ src/favicon_extractor/test_favicon_extractor.py
@@ -1,7 +1,7 @@
 import unittest
 
-from fixtures import *
-from favicon_extractor import get_favicon
+#from fixtures import *
+#from favicon_extractor import get_favicon
 
 
 class TestFaviconMethods(unittest.TestCase):
@@ -10,6 +10,22 @@ class TestFaviconMethods(unittest.TestCase):
     def test_has_no_favicon_defined_and_no_favicon_on_server(self):
         result = get_favicon('amazon.com')
     """
+    def test_check_dns(self):
+        pass
+
+    def test_calc_href(self):
+        pass
+
+    def test_poke_url_timesout(self):
+        pass
+
+    def test_poke_url_finds_favicon(self):
+        pass
+
+    def test_poke_url_does_not_find_favicon(self):
+        pass
+
+    
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
